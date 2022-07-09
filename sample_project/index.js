b6e6d81e9a96cfc8976b4767cd91a06ea4df211a
indal <indalkumar56@gmail.com>
2022-07-09 06:35:31 +0530

    sample project

---
+++ sample_project/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("hello world!!");
+  res.send("Hello world!!");
+});
+
+app.listen(3000, () => {
+  console.log("listening on port --> 3000");
+});
